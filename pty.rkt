fee11580361241af4715ffa07dd4e17e1088fa27
William G Hatch <willghatch@gmail.com>
2015-06-04 12:36:47 -0600

    use extra argument on scheme_make_fd_output_port
    This is necessary on the computer I'm using right now (x86), but it was
    not necessary on my normal computer (x86_64).  They are both up-to-date
    Arch Linux installations, so they should have the same version of
    Racket.  I originally tried using the extra argument and it failed on my
    normal computer... but maybe I'm just crazy.  It works fine on my wife's
    x86_64 Ubuntu install.  I guess if there are problems with it later I'll
    fix it then...

--- pty.rkt
+++ pty.rkt
@@ -39,7 +39,7 @@
 
 (define scheme_make_fd_output_port
   (get-ffi-obj "scheme_make_fd_output_port" #f
-               (_fun _int _scheme _int _int -> _scheme)))
+               (_fun _int _scheme _int _int _int -> _scheme)))
 (define scheme_make_fd_input_port
   (get-ffi-obj "scheme_make_fd_input_port" #f
                (_fun _int _scheme _int _int -> _scheme)))
@@ -57,8 +57,8 @@
 (define (my-openpty [width 80] [height 24])
   (let ((ws (new-winsize width height)))
     (define-values (master slave slave-name) (openpty #f #f ws))
-    (define-values (m-in m-out) (scheme_make_fd_output_port master "mastername" 0 0))
-    (define-values (s-in s-out) (scheme_make_fd_output_port slave "slavename" 0 0))
+    (define-values (m-in m-out) (scheme_make_fd_output_port master "mastername" 0 0 1))
+    (define-values (s-in s-out) (scheme_make_fd_output_port slave "slavename" 0 0 1))
     (values m-in m-out s-in s-out master slave)))
 
 
