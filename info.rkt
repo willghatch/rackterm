5cab040710601ddf8e827c64f324bfc9b2839116
William G Hatch <willghatch@gmail.com>
2015-12-13 22:48:10 -0700

    update dependencies

--- info.rkt
+++ info.rkt
@@ -2,6 +2,7 @@
 (define deps '("base"
                "draw-lib"
                "gui-lib"
+               "rackunit-lib"
                "scheme-lib"))
 (define racket-launcher-names '("rackterm-xterm"))
 (define racket-launcher-libraries '("xterm.rkt"))
