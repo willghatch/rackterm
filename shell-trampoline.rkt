56533f944b19623a7b90c0ec3f62269640503346
William G Hatch <william@hatch.uno>
2016-05-20 12:46:59 -0600

    put shell trampoline code in module+ main

--- shell-trampoline.rkt
+++ shell-trampoline.rkt
@@ -4,21 +4,22 @@
 ;; control and let everything work properly in a terminal.  My idea was to have
 ;; this in racket so no code in other languages would be needed.
 
-(require "pty.rkt")
+(module+ main
+  (require "pty.rkt")
 
-(define args (vector->list (current-command-line-arguments)))
+  (define args (vector->list (current-command-line-arguments)))
 
-;; this gives the process a new session ID
-(setsid)
+  ;; this gives the process a new session ID
+  (setsid)
 
-;; Some terminal emulators use setpgid too, but it's failing for me
-;(setpgid 0 0)
+  ;; Some terminal emulators use setpgid too, but it's failing for me
+  ;(setpgid 0 0)
 
-;; This makes it so emacs won't say "Could not open file: /dev/tty".
-;; So far it's the only program that runs differently
-(set-controlling-tty (scheme_get_port_file_descriptor (current-input-port)))
+  ;; This makes it so emacs won't say "Could not open file: /dev/tty".
+  ;; So far it's the only program that runs differently
+  (set-controlling-tty (scheme_get_port_file_descriptor (current-input-port)))
 
-(with-handlers ([(lambda (exn) #t)
-                 (lambda (exn) ((error-display-handler) (exn-message exn) exn))])
-  (apply execvp args))
+  (with-handlers ([(lambda (exn) #t)
+                   (lambda (exn) ((error-display-handler) (exn-message exn) exn))])
+    (apply execvp args)))
 
